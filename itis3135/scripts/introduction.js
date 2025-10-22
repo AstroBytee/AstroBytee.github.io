098bf9d533eeb278e64d76ce3b6f90ce55baa3de
Ryan L <ryan.lac5@icloud.com>
2025-10-22 16:12:17 -0400

    default image in form is working now.
    also courses now reset and clear properly

--- itis3135/scripts/introduction.js
+++ itis3135/scripts/introduction.js
@@ -1,6 +1,9 @@
 let courseCount = 5; // Start from 5 since there are 5 prefilled courses
 
-// Load and display user-uploaded image
+// prevents page refresh / default behavior on form submission
+document.getElementsByTagName("form")[0].addEventListener("submit", (e) => {e.preventDefault();});
+
+// LOAD and display user-uploaded IMAGE
 document.getElementById("pictureUpload").addEventListener("change", () => {
     let img = document.getElementById("pictureUpload").files[0];
 
@@ -11,7 +14,7 @@ document.getElementById("pictureUpload").addEventListener("change", () => {
     document.getElementById("loadImage").innerHTML = text;
 });
 
-// Clear form fields when 'Clear' button is clicked
+// CLEAR form when 'Clear' button is clicked
 document
   .querySelector("#clearButton")
   .addEventListener("click", () => {
@@ -21,9 +24,12 @@ document
     });
     // Clear image display area
     document.getElementById("loadImage").innerHTML = "";
+    // Clear courses
+    document.getElementById("coursesContainer").innerHTML = "";
+    courseCount = 0;
   });
 
-// Add default image when "Reset" button is clicked
+// RESET form when 'Reset' button is clicked
 document
   .querySelector("button[type='reset']")
   .addEventListener("click", () => {
@@ -41,9 +47,94 @@ document
     let defaultImageURL = "images/self_at_restaraunt.png";
     let text = "<img src='" + defaultImageURL + "' alt='Me at a Mediterranean Restaurant'>";
     document.getElementById("loadImage").innerHTML = text;
+    // Add default courses and reset course count
+    document.getElementById("coursesContainer").innerHTML = `
+      <!-- Prefilled Courses: -->
+      <!-- Course 1: -->
+      <fieldset id="course1">
+          <label for="department1">Department:</label>
+          <input type="text" id="department1" name="department1" placeholder="Enter course department here" value="STAT" required/>
+          
+          <label for="courseNumber1">Course Number:</label>
+          <input type="text" id="courseNumber1" name="courseNumber1" placeholder="Enter course number here" value="2122" required/>
+          
+          <label for="courseName1">Course Name:</label>
+          <input type="text" id="courseName1" name="courseName1" placeholder="Enter course name here" value="Intro to Prob & Stat" required/>
+          
+          <label for="reasonForTaking1">Reason for Taking:</label>
+          <input type="text" id="reasonForTaking1" name="reasonForTaking1" placeholder="Enter reason for taking course here" value="Required course." required/>
+          
+          <button type="button" class="removeCourseButton">Remove Course</button>
+      </fieldset>
+      <!-- Course 2: -->
+      <fieldset id="course2">
+          <label for="department2">Department:</label>
+          <input type="text" id="department2" name="department2" placeholder="Enter course department here" value="ITIS" required/>
+          
+          <label for="courseNumber2">Course Number:</label>
+          <input type="text" id="courseNumber2" name="courseNumber2" placeholder="Enter course number here" value="3135" required/>
+          
+          <label for="courseName2">Course Name:</label>
+          <input type="text" id="courseName2" name="courseName2" placeholder="Enter course name here" value="Front-End Web Application Development" required/>
+          
+          <label for="reasonForTaking2">Reason for Taking:</label>
+          <input type="text" id="reasonForTaking2" name="reasonForTaking2" placeholder="Enter reason for taking course here" value="I wanted to learn more about front-end web development. I've only touched the basics and I'm excited to learn more." required/>
+          
+          <button type="button" class="removeCourseButton">Remove Course</button>
+      </fieldset>
+      <!-- Course 3: -->
+      <fieldset id="course3">
+          <label for="department3">Department:</label>
+          <input type="text" id="department3" name="department3" placeholder="Enter course department here" value="ITSC" required/>
+          
+          <label for="courseNumber3">Course Number:</label>
+          <input type="text" id="courseNumber3" name="courseNumber3" placeholder="Enter course number here" value="2181" required/>
+          
+          <label for="courseName3">Course Name:</label>
+          <input type="text" id="courseName3" name="courseName3" placeholder="Enter course name here" value="Introduction to Computer Systems" required/>
+          
+          <label for="reasonForTaking3">Reason for Taking:</label>
+          <input type="text" id="reasonForTaking3" name="reasonForTaking3" placeholder="Enter reason for taking course here" value="Required Course. However, I'm still looking forward to it." required/>
+          
+          <button type="button" class="removeCourseButton">Remove Course</button>
+      </fieldset>
+      <!-- Course 4: -->
+      <fieldset id="course4">
+          <label for="department4">Department:</label>
+          <input type="text" id="department4" name="department4" placeholder="Enter course department here" value="ARTA" required/>
+          
+          <label for="courseNumber4">Course Number:</label>
+          <input type="text" id="courseNumber4" name="courseNumber4" placeholder="Enter course number here" value="1502" required/>
+          
+          <label for="courseName4">Course Name:</label>
+          <input type="text" id="courseName4" name="courseName4" placeholder="Enter course name here" value="Global Arts/Humanities: Art in a Global Context" required/>
+          
+          <label for="reasonForTaking4">Reason for Taking:</label>
+          <input type="text" id="reasonForTaking4" name="reasonForTaking4" placeholder="Enter reason for taking course here" value="I needed a class to fulfill one of the Theme requirements for my major. I thought it would be easy and I hope I will find it somewhat interesting." required/>
+          
+          <button type="button" class="removeCourseButton">Remove Course</button>
+      </fieldset>
+      <!-- Course 5: -->
+      <fieldset id="course5">
+          <label for="department5">Department:</label>
+          <input type="text" id="department5" name="department5" placeholder="Enter course department here" value="MUSC" required/>
+          
+          <label for="courseNumber5">Course Number:</label>
+          <input type="text" id="courseNumber5" name="courseNumber5" placeholder="Enter course number here" value="1512" required/>
+          
+          <label for="courseName5">Course Name:</label>
+          <input type="text" id="courseName5" name="courseName5" placeholder="Enter course name here" value="Local Arts/Humanities: Music in U.S. Communities" required/>
+          
+          <label for="reasonForTaking5">Reason for Taking:</label>
+          <input type="text" id="reasonForTaking5" name="reasonForTaking5" placeholder="Enter reason for taking course here" value="Same as ARTA 1502. I needed one more class to fulfill my Theme requirements for my major." required/>
+          
+          <button type="button" class="removeCourseButton">Remove Course</button>
+      </fieldset>
+    `;
+    courseCount = 5;
 });
 
-// Remove course when 'Remove Course' button is clicked
+// REMOVE COURSE when 'Remove Course' button is clicked
 document
   .querySelector("#coursesContainer")
   .addEventListener("click", (event) => {
@@ -71,7 +162,7 @@ document
     }
   });
 
-// Add another course input field when 'Add Course' button is clicked
+// ADD COURSE input field when 'Add Course' button is clicked
 document
   .querySelector("#addCourseButton")
   .addEventListener("click", () => {
@@ -93,17 +184,7 @@ document
     coursesContainer.appendChild(newCourseInput);
   });
 
-// Get introduction information from form data
-function getIntroductionInformation() {
-    const formData = new FormData(document.getElementsByTagName("form")[0]);
-    const introduction = {};
-    formData.forEach((value, key) => {
-        introduction[key] = value;
-    });
-    return introduction;
-}
-
-// update page to show introduction using form data when 'Submit' button is clicked
+// LOAD INTRODUCTION using form data when 'Submit' button is clicked
 document
   .querySelector("button[type='submit']")
   .addEventListener("click", () => {
@@ -117,7 +198,12 @@ document
       }
     }
 
-    const introduction = getIntroductionInformation();
+    // get introduction information from form data
+    const formData = new FormData(document.getElementsByTagName("form")[0]);
+    const introduction = {};
+    formData.forEach((value, key) => {
+        introduction[key] = value;
+    });
     // Build the content string with introduction details
     let content = `
         <h2>Introduction</h2>
@@ -176,6 +262,3 @@ document
     // Update the output container with the introduction content
     document.getElementById("outputContainer").innerHTML = content;
   });
-
-// prevents page refresh / default behavior on form submission
-document.getElementsByTagName("form")[0].addEventListener("submit", (e) => {e.preventDefault();});
